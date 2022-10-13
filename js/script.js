616cd0b407417f2b034916ca53dfeeebe0e2ecc0
Omar-Codess <omartaki8@gmail.com>
2022-10-13 19:34:51 +0200

    push generico

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ const list = ["mele", "pere", "pane", "pasta", "latte", "avena"]
 console.log(list);
 
 let i = 0;
+
 while (i < list.length){
     console.log(list[i]);
+    const result = document.querySelector("li");
+    result.innerHTML = list[i]
     i++;
-}
 
+}
